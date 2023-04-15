20584faba6e60538605ae1db846f345b723072a4
bnshahdev <bnshah.dev@gmail.com>
2023-04-15 17:09:13 -0400

    parcel, scripts added

--- using-react/App.js
+++ using-react/App.js
@@ -1,3 +1,10 @@
-const h1 = React.createElement("h1", {}, "Hands on recap - react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const div = React.createElement("div", {}, [
+  React.createElement("h1", {}, "1 - H1"),
+  React.createElement("h1", {}, "2 - H1"),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root-react"));
-root.render(h1);
+root.render(div);
